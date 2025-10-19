fc66d6a9a9056a8400cb6c89215ffa5e75617504
Aaditya Vikram <aadityavikram628@gmail.com>
2025-10-19 20:48:29 +0530

    Removed port forwarding as it was not required

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, render_template_string
 import threading
-import time
-import webbrowser
 import paramiko
-import socket
-import select
 
 app = Flask(__name__)
 
@@ -73,9 +69,9 @@ def index():
         password = request.form["password"]
 
         delay_seconds = 1
+        server_ip = request.host.split(':')[0]
 
         threading.Thread(target=start_server, args=(username, ip, password), daemon=True).start()
-        threading.Thread(target=port_forward, args=(username, ip, password), daemon=True).start()
 
         return f"""
            <html>
@@ -83,7 +79,7 @@ def index():
                <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
                <script>
                  setTimeout(function() {{
-                   window.location.href = "http://localhost:8888";
+                   window.location.href = "http://{server_ip}:8888";
                  }}, {delay_seconds * 1000});
                </script>
              </head>
@@ -115,79 +111,5 @@ def start_server(username, ip, password):
         print("Failed to start NAS server:", e)
 
 
-def port_forward(username, ip, password):
-    try:
-        print("Setting up port forwarding...")
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(ip, username=username, password=password)
-
-        # Local port 8888 forwarded to remote localhost:8888
-        transport = client.get_transport()
-        local_port = 8888
-        remote_host = 'localhost'
-        remote_port = 8888
-
-        # Create a socket on local machine and forward data between this and remote server
-        class ForwardServer (threading.Thread):
-            def __init__(self):
-                threading.Thread.__init__(self)
-                self.daemon = True
-
-            def run(self):
-                try:
-                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    sock.bind(('0.0.0.0', local_port))
-                    sock.listen(5)
-                    print(f"Port forwarding: localhost:{local_port} -> {remote_host}:{remote_port} on {ip}")
-
-                    while True:
-                        client_sock, addr = sock.accept()
-                        print(f"Incoming connection from {addr}")
-                        # Start forwarding in a new thread
-                        threading.Thread(target=handler, args=(client_sock,), daemon=True).start()
-                except Exception as e:
-                    print("Forward server error:", e)
-
-        def handler(client_sock):
-            try:
-                chan = transport.open_channel('direct-tcpip', (remote_host, remote_port), client_sock.getsockname())
-            except Exception as e:
-                print("Failed to open channel:", e)
-                client_sock.close()
-                return
-
-            if chan is None:
-                print("Failed to open channel.")
-                client_sock.close()
-                return
-
-            while True:
-                r, w, x = select.select([client_sock, chan], [], [])
-                if client_sock in r:
-                    data = client_sock.recv(1024)
-                    if len(data) == 0:
-                        break
-                    chan.send(data)
-                if chan in r:
-                    data = chan.recv(1024)
-                    if len(data) == 0:
-                        break
-                    client_sock.send(data)
-
-            chan.close()
-            client_sock.close()
-
-        forward_server = ForwardServer()
-        forward_server.start()
-        # Keep the forwarding thread running
-        forward_server.join()
-        client.close()
-
-    except Exception as e:
-        print("Failed to setup port forwarding:", e)
-
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000)
